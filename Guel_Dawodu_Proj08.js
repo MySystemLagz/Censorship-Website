4be2cd7f19795f4c6e0a8b36be95fd8e65fc4029
MySystemLagz <43154408+MySystemLagz@users.noreply.github.com>
2019-05-01 08:42:53 -0500

    Add files via upload

---
+++ Guel_Dawodu_Proj08.js
@@ -0,0 +1,15 @@
+function easterEgg() {
+    var searchInput = document.getElementById('search-bar').value;
+
+    if (searchInput == '69') {
+       window.alert('NICE'); 
+    } 
+    
+    else if (searchInput == '420') {
+        window.alert('blaze it!');
+    }
+
+    else if (searchInput == 'minecraft') {
+        window.location.replace('https://youtu.be/dgha9S39Y6M');
+    }
+}
